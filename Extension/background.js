9e61a2db532a65a347e0c9a074becae9bb7efae8
Yuhui Li <a@yuhuili.com>
2016-08-21 10:17:33 -0700

    Handle ad url that is created by reloading

--- Extension/background.js
+++ Extension/background.js
@@ -2,4 +2,10 @@ chrome.tabs.onCreated.addListener(function(newTab) {
   if (newTab.url.includes("adv.videomega.tv")) {
     chrome.tabs.remove(newTab.id);
   }
-}
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (tab.url.includes("adv.videomega.tv")) {
+    chrome.tabs.remove(tab.id);
+  }
+});
